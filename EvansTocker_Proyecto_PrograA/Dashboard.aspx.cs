289eb19047ff1be5b5d253c531781f6de3bd0dea
Tocker04 <evanstocvs@gmail.com>
2025-08-06 21:07:36 -0600

    Avance en Agregar Cita de Dashboard

--- EvansTocker_Proyecto_PrograA/Dashboard.aspx.cs
+++ EvansTocker_Proyecto_PrograA/Dashboard.aspx.cs
@@ -5,10 +5,26 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using BarberCode_BLL;
+using BarberCode_BLL.Modelo;
+
+
+//Para enviar SMS de confirmacion con API Twilio
+using System.Configuration;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+using WebListItem = System.Web.UI.WebControls.ListItem;
+
 namespace EvansTocker_Proyecto_PrograA
 {
     public partial class Dashboard : Page
     {
+        //Servicios//
+        private static List<ServicioDTO> ServicioSS;
+        //Usuarios barberos//
+        private static List<UsuarioDTO> UsuarioO;
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
@@ -22,6 +38,9 @@ namespace EvansTocker_Proyecto_PrograA
                     // Redirige si no hay sesión activa
                     Response.Redirect("Login.aspx");
                 }
+
+                CargarServicios();//cargar la los datos de la bd (tabla Citas) a la pagina de gestionar Citas
+                CargarBarberos(); //cargar los barberos registrados de la bd
             }
         }
 
@@ -30,6 +49,44 @@ namespace EvansTocker_Proyecto_PrograA
             //aun no tiene nada
 
         }
+
+        //0)Metodo para cargar la tabla(de BD) de Servicios en el dllBarbero y obtener el nombre
+        private void CargarServicios()
+        {
+            ServicioSS = ServicioService.ConsultarServicios();
+            ddlServicio.Items.Clear();
+
+            // Agregar manualmente la opción por defecto
+            ddlServicio.Items.Add(new WebListItem("Seleccionar Servicio", ""));
+            foreach (ServicioDTO Servicio in ServicioSS)
+            {
+                ddlServicio.Items.Add(Servicio.Nombre);
+                //ddlServicio.Items.Add(new WebListItem(Servicio.Nombre, Servicio.ServicioId.ToString()));
+
+            }
+        }
+
+        //0)Metodo para cargar la tabla(de BD) de Usuarios en el dllBarbero y obtener el nombre
+        private void CargarBarberos()
+        {
+            UsuarioO = UsuarioService.ConsultarUsuarios();
+            ddlBarbero.Items.Clear();
+
+            // Agregar manualmente la opción por defecto
+            ddlBarbero.Items.Add(new WebListItem("Seleccionar Barbero", ""));
+            foreach (UsuarioDTO Usuario in UsuarioO)
+            {
+                // Verificamos que el rol del usuario sea "barbero" (rolId = 2)
+                if (Usuario.RolesId != null && Usuario.RolesId.RolId == 2)
+                {
+                    ddlBarbero.Items.Add(Usuario.Nombre);
+                    //ddlBarbero.Items.Add(new WebListItem(Usuario.Nombre, Usuario.UsuarioId.ToString()));
+                }
+            }
+        }
+
+
+
 
         protected void BtnCerrarSesion_Click(object sender, EventArgs e)
         {
--- EvansTocker_Proyecto_PrograA/Dashboard.aspx.designer.cs
+++ EvansTocker_Proyecto_PrograA/Dashboard.aspx.designer.cs
@@ -78,13 +78,13 @@ namespace EvansTocker_Proyecto_PrograA
         protected global::System.Web.UI.WebControls.TextBox txtHora;
 
         /// <summary>
-        /// Control ddlBarberoCliente.
+        /// Control ddlBarbero.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddlBarberoCliente;
+        protected global::System.Web.UI.WebControls.DropDownList ddlBarbero;
 
         /// <summary>
         /// Control btnAgregarCita.
